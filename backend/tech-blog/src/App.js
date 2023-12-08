444beb8fdffbf63c74fbc93d1399ae170ab269f3
Niko <nicosejohnsoriano@gmail.com>
2023-12-09 02:43:50 +0800

    Update backend and tech-blog code

--- backend/tech-blog/src/App.js
+++ backend/tech-blog/src/App.js
@@ -3,6 +3,9 @@ import Home from './pages/main/Home'
 import Login from './pages/auth/Login'
 import Signup from './pages/auth/Signup'
 import Landing from './pages/main/Landing'
+import ForgotPassword from './pages/auth/forgot-password'
+import OTP from './pages/auth/otp'
+import ChangePassword from './pages/auth/change-password'
 import './App.css';
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
         <Route path="/main/Home" element={<Home/>}/>
         <Route path="/auth/Login" element={<Login/>}/>
         <Route path="/auth/Signup" element={<Signup/>}/>
+        <Route path="/auth/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/auth/forgot-password/otp" element={<OTP/>}/>
+        <Route path="/auth/change-password" element={<ChangePassword/>}/>
      </Routes>
    </>
   );
