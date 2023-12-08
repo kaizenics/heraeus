7d724cb4e2d8dd110ab056540a906f3baa289364
Niko <nicosejohnsoriano@gmail.com>
2023-12-09 02:43:06 +0800

    Add django_otp and email configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_otp',
+    'django_otp.plugins.otp_totp',
     'rest_framework',
     'corsheaders',
     'api',
@@ -128,10 +130,17 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'  # Corrected line
+MEDIA_URL = '/media/' 
 
 CORS_ALLOW_ALL_ORIGINS = True
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'heraeus.interactive@gmail.com'
+EMAIL_HOST_PASSWORD = 'ydae kulz ytbn vhno'
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,4 @@
-# project/urls.py
+# backend/urls.py
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
